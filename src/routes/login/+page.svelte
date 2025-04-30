bbdbae52774c8a8e6922843800da9d3114d06c2d
Myoungjun Ko <kmj951015@gmail.com>
2025-05-01 02:42:27 +0900

    :art: login, signup 동작 분리

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,36 +1,17 @@
 <script lang="ts">
 	import AuthForm from '$lib/components/AuthForm.svelte';
-	import { apiURL } from '$lib/config';
+	import { login } from '$lib/api/auth';
+	import { goto } from '$app/navigation';
+	import type { LoginPayload } from '$lib/types';
 
-	interface LoginPayload {
-		username: string;
-		password: string;
-	}
-
-	function handleLogin({ username, password }: LoginPayload) {
-		const formData = new FormData();
-		formData.append('username', username);
-		formData.append('password', password);
-
-		fetch(apiURL.user.login, {
-			method: 'POST',
-			body: formData
-		})
-			.then(async res => {
-				const data = await res.json();
-				if (!res.ok) throw new Error(data);
-				return data;
-			})
-			.then(data => {
-				console.log('로그인 성공!', data);
-				// 예: 토큰 저장 및 페이지 이동
-			})
-			.catch(err => {
-				console.log("error: ", err);
-				const errMsg = "로그인 실패!"
-				alert(errMsg);
-				console.error(errMsg);
-			});
+	async function handleLogin(payload: LoginPayload) {
+		try {
+			await login(payload);
+			await goto('/');
+		} catch (err) {
+			alert('로그인 실패');
+			console.error(err);
+		}
 	}
 </script>
 
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,44 +1,17 @@
 <script lang="ts">
 	import AuthForm from '$lib/components/AuthForm.svelte';
-	import { apiURL } from '$lib/config';
+	import { signup } from '$lib/api/auth';
+	import { goto } from '$app/navigation';
+	import type { SignUpPayload } from '$lib/types';
 
-	interface SignUpPayload {
-		username: string;
-		email: string;
-		password: string;
-		confirmPassword: string;
-	}
-
-	function handleSignUp({ username, email, password, confirmPassword }: SignUpPayload) {
-		const jsonData = {
-			'name': username,
-			'email': email,
-			'password': password,
-			'check_password': confirmPassword
-		};
-
-		fetch(apiURL.user.signup, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(jsonData)
-		})
-			.then(async res => {
-				const data = await res.json();
-				if (!res.ok) throw new Error(data);
-				return data;
-			})
-			.then(data => {
-				console.log('회원가입 성공!', data);
-				// 예: 토큰 저장 및 페이지 이동
-			})
-			.catch(err => {
-				console.log("error: ", err);
-				const errMsg = "회원가입 실패"
-				alert(errMsg);
-				console.error(errMsg);
-			});
+	async function handleSignUp(payload: SignUpPayload) {
+		try {
+			await signup(payload);
+			await goto('/login');
+		} catch (err) {
+			alert('회원가입 실패');
+			console.error(err);
+		}
 	}
 </script>
 
