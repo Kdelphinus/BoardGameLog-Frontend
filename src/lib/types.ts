bfdee468966228e62659c9a272aefb281bd0a6a5
Ko Myoung Jun <kmj951015@gmail.com>
2025-05-16 20:26:13 +0900

    :label: Game type 추가

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,19 +2,19 @@ export type AuthState = {
 	isLoggedIn: boolean;
 	accessToken: string | null;
 	name: string | null;
-}
+};
 
 export type LoginPayload = {
 	username: string;
 	password: string;
-}
+};
 
 export type SignUpPayload = {
 	username: string;
 	email: string;
 	password: string;
 	confirmPassword: string;
-}
+};
 
 export type Post = {
 	id: number;
@@ -25,4 +25,9 @@ export type Post = {
 	subject: string;
 	content?: string | null;
 	picture?: string | null;
+};
+
+export interface Game {
+	id: number;
+	name: string;
 }
